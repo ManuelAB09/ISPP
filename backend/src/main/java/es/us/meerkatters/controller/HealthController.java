9d7c799711479e65fbe82ab15f766dbc0a4b34d9
Mancalrod <mancalrod@alum.us.es>
2026-02-20 00:33:46 +0100

    fix: arreglar linter

--- backend/src/main/java/es/us/meerkatters/controller/HealthController.java
+++ backend/src/main/java/es/us/meerkatters/controller/HealthController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
-import java.util.Map;
 
 /**
  * Controlador de ejemplo para verificar que Swagger funciona.
