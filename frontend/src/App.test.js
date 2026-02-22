b01c638bf7e8028c95ff576a2b1cda1360bec142
Mancalrod <mancalrod@alum.us.es>
2026-02-22 20:49:57 +0100

    fix: Correccion de problemas de CI y primer commit de backend

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders MeerKatters Home', () => {
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
+  const heading = screen.getByText(/MeerKatters Home/i);
+  expect(heading).toBeInTheDocument();
 });
