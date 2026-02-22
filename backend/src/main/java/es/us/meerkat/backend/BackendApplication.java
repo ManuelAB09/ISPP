32a61c0d07e92cf49355517d3bd82d44205181cf
Mancalrod <mancalrod@alum.us.es>
2026-02-22 20:57:13 +0100

    fix: Arreglo CI Backend

--- backend/src/main/java/es/us/meerkat/backend/BackendApplication.java
+++ backend/src/main/java/es/us/meerkat/backend/BackendApplication.java
@@ -1,26 +1,18 @@
-
-/**
- * Main Spring Boot application for Meerkat Backend.
- */
 package es.us.meerkat.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-
 /**
- * Utility class for starting the Spring Boot application.
+ * Main Spring Boot application for Meerkat Backend.
  */
-public final class BackendApplication {
-
-
-	/**
-	 * Main entry point for the Spring Boot application.
-	 * @param args the command line arguments
-	 */
-	public static void main(final String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
-
+@SpringBootApplication
+public class BackendApplication {
+    /**
+     * Main entry point for the Spring Boot application.
+     * @param args the command line arguments
+     */
+    public static void main(final String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
 }
--- backend/src/main/java/es/us/meerkat/backend/package-info.java
+++ backend/src/main/java/es/us/meerkat/backend/package-info.java
@@ -1,4 +1,4 @@
 /**
- * Package info for es.us.meerkat.backend
+ * Package info for es.us.meerkat.backend.
  */
 package es.us.meerkat.backend;
