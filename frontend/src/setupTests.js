24547e1953935f0c18d57151667b4f7737af6bf3
BPP4634 <beagutara@alum.us.es>
2026-02-26 05:20:54 +0100

    fix: Arreglando CI de tests de frontend
    Refs: #130

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -3,3 +3,20 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// Mock react-leaflet and leaflet to avoid ES6 module parsing issues in Jest
+jest.mock('react-leaflet', () => ({
+  MapContainer: ({ children }) => <div data-testid="map-container">{children}</div>,
+  TileLayer: () => null,
+  Marker: () => null,
+  Popup: () => null,
+  useMapEvents: () => ({}),
+  useMap: () => ({
+    setView: jest.fn(),
+  }),
+}));
+
+jest.mock('leaflet', () => ({
+  icon: jest.fn(() => ({})),
+  divIcon: jest.fn(() => ({})),
+}));
